ff18d1d0c8a90de44d1232048016028187d022ac
Yuval Allweil <themarquisdesheric@gmail.com>
2020-11-22 09:50:06 -0800

    install tailwind typography on everything (test)

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -17,6 +17,6 @@
 
 <Nav {segment}/>
 
-<main>
+<main class="prose">
 	<slot></slot>
 </main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,3 @@
-<style>
-	h1 {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
 	<title>Specimen Magazine</title>
 </svelte:head>
