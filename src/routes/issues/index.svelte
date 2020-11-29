a2a82fce3fa2001e9084deabf553e0efb92d7360
Yuval Allweil <themarquisdesheric@gmail.com>
2020-11-28 21:40:06 -0800

    style issues page for multiple issue cards

--- src/routes/issues/index.svelte
+++ src/routes/issues/index.svelte
@@ -17,34 +17,39 @@
 <div class="w-full">
   <h1 class="text-lg font-light pb-2">Issues</h1>
   
-  <a href="/issues/{number}" rel="prefetch">
-    <div class="issue-card border rounded text-sm font-light p-4">
-      <header class="mb-2">
-        <h2 class="flex justify-between text-xl">
-          <span class="font-bold">{title}</span>
-          <span class="font-extralight">{date}</span>
-        </h2>
-        <p class="leading-4">Issue №{number}</p>
-      </header>
-      <hr class="pb-1" />
-      {#each professors as professor, index}
-        <section class="py-1 flex justify-between items-center">
-          <a href="/interviews/{professor.slug}" rel="prefetch">
-            <p>{professor.name}</p>
-          </a>
-          <Tag tag={professor.tags[0]} />
-        </section>
-        {#if index !== professors.length - 1}
-          <hr class="my-1" />
-        {/if}
-      {/each}
-    </div>
-  </a>
+  <div class="flex flex-wrap">
+    {#each [1,2,3,4,5] as _}
+      <a href="/issues/{number}" rel="prefetch" class="issue-card">
+        <div class="w-full border rounded text-sm font-light p-4">
+          <header class="mb-2">
+            <h2 class="flex justify-between text-xl">
+              <span class="font-bold">{title}</span>
+              <span class="font-extralight">{date}</span>
+            </h2>
+            <p class="leading-4">Issue №{number}</p>
+          </header>
+          <hr class="pb-1" />
+          {#each professors as professor, index}
+            <section class="py-1 flex justify-between items-center">
+              <a href="/interviews/{professor.slug}" rel="prefetch">
+                <p>{professor.name}</p>
+              </a>
+              <Tag tag={professor.tags[0]} />
+            </section>
+            {#if index !== professors.length - 1}
+              <hr class="my-1" />
+            {/if}
+          {/each}
+        </div>
+      </a>
+    {/each}
+  </div>
 </div>
 
 <style>
-  .issue-card {
+  .issue-card div {
     max-width: 315px;
+    margin: 0 3rem 1rem 0;
   }
 
   .issue-card header p,
