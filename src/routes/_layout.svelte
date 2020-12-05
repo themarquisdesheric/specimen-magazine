b6b72ffcd94b5e88db2b3a5fae7f447d6e9aae03
Yuval Allweil <themarquisdesheric@gmail.com>
2020-12-04 23:32:49 -0800

    add social links to footer

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Nav from '../components/Nav.svelte'
+	import instagram from '../../static/instagram.svg'
 
 	export let segment: string
 </script>
@@ -9,6 +10,18 @@
 	<slot />
 </main>
 <footer class="text-center py-2 font-extralight">
+	<div class="text-center mt-8">
+		<span class="mr-4">
+			<a href="https://twitter.com/specimen_mag?lang=en" target="_blank" rel="noopener noreferrer">
+				<img src="twitter.svg" alt="twitter" />
+			</a>
+		</span>
+		<span>
+			<a href="https://www.instagram.com/specimenmagazine/" target="_blank" rel="noopener noreferrer">
+				<img src="instagram.svg" alt="instagram" />
+			</a>
+		</span>
+	</div>
 	<small>Specimen Magazine © 2011 - 2015</small>
 </footer>
 
@@ -16,9 +29,20 @@
 	main {
 		margin-top: 57px;
 		/* 100vh - header - footer */
-		min-height: calc(100vh - 56px - 40px);
+		min-height: calc(100vh - 56px - 110px);
 		max-width: 56em;
 		background-color: white;
 		box-sizing: border-box;
+	}
+
+	span {
+		display: inline-block;
+		background: black;
+		border-radius: 50%;
+	}
+
+	img {
+		width: 2rem;
+    padding: .6rem;
 	}
 </style>
