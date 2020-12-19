31c5e6f08605c48fdb35ff7b0f3160734904873c
Yuval Allweil <themarquisdesheric@gmail.com>
2020-12-19 15:04:19 -0800

    style IssueCard; prepare it for color palette

--- src/components/IssueCard.svelte
+++ src/components/IssueCard.svelte
@@ -10,16 +10,18 @@
     date,
     professors,
   } = issue
-
   const MAX_PROFESSOR_LENGTH = 5
+  let headerWidth
 </script>
 
 <a href="/issues/{number}" rel="prefetch" class="issue-card w-full border rounded text-sm p-4 mb-4">
-  <header class="mb-3">
-    <h2 class="flex justify-between text-xl">
-      <span class="font-bold">{title}</span>
-      <span class="font-extralight">{date}</span>
-    </h2>
+  <header
+    bind:clientWidth={headerWidth}
+    style="--headerWidth: {headerWidth}px"
+    class="relative mb-4 text-white"
+  >
+    <h2 class="absolute flex justify-between font-bold text-xl">{title}</h2>
+    <span class="absolute text-xl font-extralight">{date}</span>
   </header>
   {#each professors as professor, index}
     {#if index === MAX_PROFESSOR_LENGTH && professors.length - 1 > MAX_PROFESSOR_LENGTH}
@@ -40,7 +42,35 @@
 </a>
 
 <style>
+  header {
+    background-color: #000;
+    height: var(--headerWidth);
+  }
+
+  h2 {
+    bottom: .5em;
+    left: .5em;
+  }
+
+  header span {
+    top: .5em;
+    right: .5em;
+  }
+
+  /* fluid font magic https://css-tricks.com/snippets/css/fluid-typography/ */
+  @media (min-width: 320px) {
+    h2,
+    header span {
+      font-size: calc(1.25rem + 6 * ((100vw - 320px) / 90));
+    }
+  }
+
   @media (min-width: 600px) {
+    h2,
+    header span {
+      font-size: 1.25rem;
+    }
+
     .issue-card {
       width: calc(50% - .5rem);
     }
